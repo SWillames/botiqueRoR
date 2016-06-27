5735078613b57a912049975f0c4de605d105160d
SWillames <sergiowoc@gmail.com>
2016-06-27 20:46:30 -0300

    Adicionando validações.

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,6 @@
 class Client < ActiveRecord::Base
+  validates_presence_of :name, :cpf, :location, :salary
+  validates_uniqueness_of :cpf
+  validates_format_of :cpf, with: /\A.[0-9]+\z/
+  validates_length_of :cpf, is: 11
 end
