4275df1cea15093002cf7160aba7ca0f96cd08b6
SWillames <sergiowoc@gmail.com>
2016-06-27 23:15:42 -0300

    Adicionando novos testes de validação.

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -21,5 +21,24 @@ describe Client do
       client = Client.create(salary: nil)
       expect(client.errors[:salary].size).to be>=1
     end
+
+    it "require valid cpf" do
+			client = Client.create(cpf: "invalid")
+			expect(client.errors[:cpf].size).to be >= 1
+		end
+
+    %w[
+		abcdfgh
+		123456789ab
+		ab145879cxv
+		@1a254#$%a!
+		12345
+		invalid
+		].each do |cpf|
+			it "requires #{cpf} as valid cpf" do
+				client = Client.create(cpf: cpf)
+				expect(client.errors[:cpf].size).to be >= 1
+			end
+		end
   end
 end
