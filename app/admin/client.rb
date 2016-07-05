2754885aa5a78d05cd99a55a5592450304738f28
SWillames <sergiowoc@gmail.com>
2016-07-05 18:28:21 -0300

    Modificações feitas na parte visual do cliente.

--- app/admin/client.rb
+++ app/admin/client.rb
@@ -1,3 +1,23 @@
 ActiveAdmin.register Client do
-  permit_params :name, :cpf, :location, :salarys
+  menu priority: 1
+  permit_params :name, :cpf, :location, :salary
+
+  index do
+    column :id
+    column :name
+    column :cpf
+    column :location
+    column :salary
+  end
+
+  form do |f|
+    f.inputs "Detalhes" do
+      f.input :name
+      f.input :cpf
+      f.input :location
+      f.input :salary
+    end
+    f.actions
+  end
+
 end
